9183023cb8474d6c147d3895152958a3a7cf9d9a
shubham mahore <shubhammahore@gmail.com>
2018-10-08 17:15:57 +0530

    commit

---
+++ app/routes/note_routes.js
@@ -0,0 +1,16 @@
+module.exports = function(app, db) {
+    const collection = 
+    app.post('/notes', (req, res) => {
+      //const note = { text: req.body.body, title: req.body.title };
+     var myobj = { name: "deviseapp", address: "delhi" };
+      db.collection('notes').insert(myobj, (err, result) => {
+        if (err) { 
+          res.send({ 'error': 'An error has occurred' }); 
+        } else {
+            console.log(" 1 Document inserted");
+          res.send(result.ops[0]);
+          db.close();
+        }
+      });
+    });
+  };
---
+++ server.js
@@ -0,0 +1,22 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const db             = require('./config/db')
+const app            = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+MongoClient.connect(db.url, (err, db) => {
+  if (err) return console.log(err)
+  var dbo = db.db("shubham");
+  console.log("Connection created");
+  require('./app/routes')(app, dbo);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  }); 
+  //db.close();
+  //console.log("Connection closed");              
+})
