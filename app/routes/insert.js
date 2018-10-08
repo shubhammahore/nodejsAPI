102e1608bbf5b41920ab0527dd1eae85b35eed6d
shubham mahore <shubhammahore@gmail.com>
2018-10-08 17:45:25 +0530

    commit

--- app/routes/insert.js
+++ app/routes/insert.js
@@ -1,15 +1,15 @@
 module.exports = function(app, db) {
     const collection = 
-    app.post('/notes', (req, res) => {
+    app.post('/notesme', (req, res) => {
       //const note = { text: req.body.body, title: req.body.title };
      var myobj = { name: "deviseapp", address: "delhi" };
-      db.collection('test').find({}).toArray((err, result) => {
+      db.collection('notes').insert(myobj, (err, result) => {
         if (err) { 
           res.send({ 'error': 'An error has occurred' }); 
         } else {
-            console.log(" found success");
-          res.send(result);
-          
+            console.log(" 1 Document inserted");
+          res.send(result.ops[0]);
+          db.close();
           
         }
       });
